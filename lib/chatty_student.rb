47183a7e64dca3e933380762d634477daa12d3ce
poignant-minifier-0983 <davidfano@gmail.com>
2019-06-11 23:50:04 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -1,5 +1,8 @@
 class ChattyStudent
   def hello
+    puts "Hey there! I'm so excited to learn stuff."
+    puts "Hey there! I'm so excited to learn stuff. How are you doing today? I'm okay, but I'm kind of tired....n, it was so crazy! What, you don't wan
+t any spoilers? Okay well let me just tell you who died..."
 
   end
 
